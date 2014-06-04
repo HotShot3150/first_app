573e747061dcceac07d13e3575accbe5c625fc3b
River Bryant <river.bryant@ibethel.org>
2014-06-04 16:44:21 -0700

    initializing first app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '95462cb755a15a119e0120f9b5cdd2b0b952eafbc34fdec18b7cc9ecd3c1398a7886268c026bd4ba5df060f981226a0e6268f70d66269747bdb6228bac3cc446'
